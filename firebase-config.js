cde80c18884553cacb1f3415283260e2fa8027d1
Arpit Gupta <your.email@example.com>
2025-08-08 22:22:44 +0400

    Add Firebase configuration for real-time multiplayer

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyASXexdZXN7mE1FAPuI1EG7clV-pDhGS14",
+  authDomain: "tic-tac-toe-multiplayer-eacd9.firebaseapp.com",
+  databaseURL: "https://tic-tac-toe-multiplayer-eacd9-default-rtdb.firebaseio.com",
+  projectId: "tic-tac-toe-multiplayer-eacd9",
+  storageBucket: "tic-tac-toe-multiplayer-eacd9.firebasestorage.app",
+  messagingSenderId: "289073556048",
+  appId: "1:289073556048:web:33b780d457e7a9ef28bf55",
+  measurementId: "G-SJ5LDFKE4E"
 };
 
 // Initialize Firebase
